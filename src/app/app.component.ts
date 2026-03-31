44a83f62eba0d711ac9f2016c7a4aa0ac52522a1
jabg97 <53027826+jabg97@users.noreply.github.com>
2026-03-31 11:52:33 -0500

    feat(auth): replace Auth0 with custom JWT session management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,26 @@
-import { Component } from '@angular/core';
-import { AuthService} from '@auth0/auth0-angular';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { AzureService } from 'src/services/azure.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(public auth: AuthService) {
-    auth.isAuthenticated$.subscribe((isAuthenticated) => {
-      if (isAuthenticated) {
-        auth.user$.subscribe((user) => {
-          localStorage.setItem('iot-auth0-user', JSON.stringify(user));
-        });
-      }else{
-        auth.loginWithRedirect()
-      }
-    });
+export class AppComponent implements OnInit {
+
+  constructor(private azureService: AzureService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Redirect to login if session is expired or absent
+    if (!this.azureService.isAuthenticated()) {
+      this.router.navigate(['/login'])
+    }
   }
 
   logOut() {
-    if(confirm('Are you sure you want to log out?')){
-      localStorage.removeItem('iot-auth0-user');
-      this.auth.logout({ logoutParams: { returnTo: document.location.origin } })
+    if (confirm('Are you sure you want to log out?')) {
+      this.azureService.logout()
     }
   }
 }
